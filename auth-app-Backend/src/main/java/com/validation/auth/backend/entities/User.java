2448c91fa688860341553dc6ff48d09b668981e2
tukaramchate <tukaramchate397@gmail.com>
2025-12-16 18:52:25 +0530

    first commit

---
+++ auth-app-Backend/src/main/java/com/validation/auth/backend/entities/User.java
@@ -0,0 +1,58 @@
+package com.validation.auth.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private UUID id;
+
+    @Column(unique = true,length = 300 )
+    private String email;
+
+    @Column(length = 300 )
+    private String name;
+
+    private String password;
+    private String image;
+    private boolean enable = true;
+
+    private Instant createdAt =  Instant.now();
+    private Instant updatedAt =  Instant.now();
+
+    @Enumerated(EnumType.STRING)
+    private Provider provider = Provider.LOCAL;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "roles_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    @PrePersist
+    protected void onCreate(){
+        Instant now = Instant.now();
+        if (createdAt == null) createdAt = now;
+        updatedAt = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        updatedAt = Instant.now();
+    }
+}
---
+++ auth-app-Backend/src/main/java/com/validation/auth/backend/services/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.validation.auth.backend.services;
+
+import com.validation.auth.backend.dtos.UserDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        return null;
+    }
+
+    @Override
+    public UserDto getUserByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public UserDto updateUser(UserDto userDto, String userId) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(UserDto userDto) {
+
+    }
+
+    @Override
+    public UserDto getUserById(String userId) {
+        return null;
+    }
+
+    @Override
+    public Iterable<UserDto> getAllUsers() {
+        return null;
+    }
+}
