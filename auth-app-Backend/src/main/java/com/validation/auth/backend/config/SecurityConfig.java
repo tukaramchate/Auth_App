e8ec88cc70c10b22ceeb2742326580bbd7f10402
tukaramchate <tukaramchate397@gmail.com>
2025-12-20 21:57:49 +0530

    added cookies and logout

--- auth-app-Backend/src/main/java/com/validation/auth/backend/config/SecurityConfig.java
+++ auth-app-Backend/src/main/java/com/validation/auth/backend/config/SecurityConfig.java
@@ -36,6 +36,8 @@ public class  SecurityConfig {
                         authorizeRequests
                                 .requestMatchers("/api/v1/auth/register").permitAll()
                                 .requestMatchers("/api/v1/auth/login").permitAll()
+                                .requestMatchers("/api/v1/auth/refresh").permitAll()
+                                .requestMatchers("/api/v1/auth/logout").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .exceptionHandling(ex ->
--- auth-app-Backend/src/main/java/com/validation/auth/backend/repositores/RefreshTokenRepository.java
+++ auth-app-Backend/src/main/java/com/validation/auth/backend/repositores/RefreshTokenRepository.java
@@ -3,8 +3,12 @@ package com.validation.auth.backend.repositores;
 import com.validation.auth.backend.entities.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
 
+    Optional<RefreshToken> findByJti(String jti);
+
 }
+
